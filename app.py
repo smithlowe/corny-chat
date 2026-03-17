838d40416ca453411b41c77e201741de4171b8f9
Your Name <mulindalawrence350@gmail.com>
2026-03-17 18:58:01 +0300

    Merged steps 2 and 3 for full timestamp support

--- app.py
+++ app.py
@@ -1,44 +1,18 @@
-import os
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_socketio import SocketIO, emit
 from datetime import datetime
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'corny_memory_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///corny.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-socketio = SocketIO(app, cors_allowed_origins="*")
-
-# This is the "Memory" structure
-class ChatMessage(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.String(50))
-    text = db.Column(db.Text)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-
-# Create the database file
-with app.app_context():
-    db.create_all()
-
-@app.route('/')
-def index():
-    # When you open the page, it gets the last 50 messages from memory
-    history = ChatMessage.query.order_by(ChatMessage.timestamp.asc()).all()
-    return render_template('index.html', history=history)
+import pytz # Optional: if you want specific Uganda time
 
 @socketio.on('message')
 def handle_message(data):
-    # 1. Save to Memory
+    # Create a nice timestamp (e.g., 06:45 PM)
+    now = datetime.now().strftime("%I:%M %p")
+    
+    # 1. Save to Database (We add the 'time' string here)
     new_msg = ChatMessage(user=data['user'], text=data['text'])
+    # Note: If your ChatMessage model doesn't have a time string yet, 
+    # we can just pass 'now' to the broadcast for now!
     db.session.add(new_msg)
     db.session.commit()
     
-    # 2. Shout to everyone
+    # 2. Add the time to the data package before shouting it out
+    data['time'] = now
     emit('message', data, broadcast=True)
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    socketio.run(app, host='0.0.0.0', port=port)
