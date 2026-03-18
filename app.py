45d2e245c573396e75c6277833bc8dd3de055437
Your Name <mulindalawrence350@gmail.com>
2026-03-18 19:06:16 +0300

    Integrated Pro features, Emoji bar, and restored Voice Notes

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'corny_secret_123'
-# CORS fix for Render deployment
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 user_count = 0
@@ -21,7 +20,6 @@ def handle_user_joined(data):
 
 @socketio.on('message')
 def handle_message(data):
-    # Pass the message AND the timestamp to everyone
     emit('render_msg', {
         'user': data['user'], 
         'content': data['content'], 
@@ -29,9 +27,18 @@ def handle_message(data):
         'type': 'text'
     }, broadcast=True)
 
+@socketio.on('voice_note')
+def handle_voice(data):
+    # This sends the audio data to everyone
+    emit('render_msg', {
+        'user': data['user'], 
+        'content': data['audio'], 
+        'time': data['time'],
+        'type': 'voice'
+    }, broadcast=True)
+
 @socketio.on('typing')
 def handle_typing(data):
-    # Sends "User is typing" to everyone EXCEPT the person typing
     emit('display_typing', data, broadcast=True, include_self=False)
 
 @socketio.on('disconnect')
